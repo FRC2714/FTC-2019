5eb81899f446758344c9bf6b3322fae20c62678b
Gagan Bhat <gaganbhat6@gmail.com>
2019-09-20 18:02:18 -0500

    .gitignore fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.Timer;
+
 @TeleOp(name = "Mecanum Drive", group = "Prototype")
 
 public class MecanumDrive extends OpMode {
@@ -12,6 +14,11 @@ public class MecanumDrive extends OpMode {
 	final static double DEADZONE = 0.01;
 	Drivetrain drivetrain;
 	static Telemetry tele;
+	double currTime;
+	double x;
+	double y;
+
+	Timer timer = new Timer();
 
 	/**
 	 * First function loaded at beginning
@@ -28,6 +35,8 @@ public class MecanumDrive extends OpMode {
 	 */
 	@Override
 	public void loop() {
+		double deltaTime = (System.nanoTime() / 1e9) - currTime;
+		currTime = System.nanoTime() / 1e9;
 		double x = gamepad1.left_stick_x,
 				y = gamepad1.left_stick_y,
 				turn = gamepad1.right_stick_x;
@@ -36,7 +45,10 @@ public class MecanumDrive extends OpMode {
 		} else {
 			drivetrain.setDirectionVector(0, 0);
 		}
+		drivetrain.updatePosition();
 		telemetry.addLine("" + x + ", " + y + ", " + turn);
-
+		telemetry.addLine("Encoder Position" + drivetrain.getLeftEncoder());
+		telemetry.addLine("L Encoder Velocity" + drivetrain.getLeftVelocity());
+		telemetry.addLine("Heading Angle" + drivetrain.odometer.getHeading());
 	}
 }
